1fe7029503b47ec6bb63fbf409ab49b3006261f2
Kelli <kellibrookelife@gmail.com>
2018-06-15 16:53:09 -0700

    add validate function

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,17 @@ Order.prototype.valueToSize = function(){
   return sizeWord;
 }
 
+function validate(uiUserName, howManyToppings){
+  if (uiUserName === "" || howManyToppings === 0){
+    alert("Please fill out the form with your name and choose at least 1 topping.");
+    document.getElementById("order-form").reset();
+  } else {
+  document.getElementById("order-form").reset();
+  $("#order-form-div").hide();
+  $("#order-confirmation-area").slideDown();
+  }
+}
+
 
 // UI LOGIC
 $(function(){
@@ -31,9 +42,9 @@ var order = new Order();
 
 $("#order-button").click(function(){
   event.preventDefault();
-  $("#order-form-div").hide();
-  order.toppings.length = 0;
   var orderName = $("#name-for-order").val();
+  var toppingsLength = order.toppings.length;
+  order.toppings.length = 0;
   $("#order-confirm-name").text(orderName);
   $("ul#toppings").text("");
   order.name = $("#name-for-order").val();
@@ -43,13 +54,13 @@ $("#order-button").click(function(){
     order.toppings.push(topping);
     $("ul#toppings").append("<li>" + topping + "</li>");
   })
-
+console.log(order.toppings.length);
   var size = order.valueToSize();
   var price = order.calculatePrice();
   $("#pizza-size").text(size);
   $("#pizza-price").text("$ " + price);
-  document.getElementById("order-form").reset();
-  $("#order-confirmation-area").slideDown();
+
+  validate(orderName, toppingsLength);
 })
 
 $("#order-again-button").click(function(){
